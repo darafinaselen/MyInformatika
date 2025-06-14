29e2f1daecdb776b02018005992ee7af31bbc866
darafinaselen <darafielena@gmail.com>
2025-06-14 08:28:21 +0800

    Splash Screen and Get Started

---
+++ app/src/main/java/com/example/myinformatika/ImageSliderAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.myinformatika
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class ImageSliderAdapter(private val images: List<Int>) : RecyclerView.Adapter<ImageSliderAdapter.ImageViewHolder>() {
+
+    inner class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.findViewById(R.id.image_view_slider)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_image_slider, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.imageView.setImageResource(images[position])
+    }
+
+    override fun getItemCount(): Int = images.size
+}
